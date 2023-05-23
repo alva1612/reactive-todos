6f2c01e160a96fb57bdce446ea1670f7291536c7
Alvaro Guillen Carbajal <aguillen@atlanticcity.local>
2023-05-23 10:07:25 -0500

    build fixed?

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import viteSvgr from "vite-plugin-svgr";
 
-console.log("wut");
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), viteSvgr()],
   build: {
